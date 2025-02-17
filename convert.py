7e32b31d854dd795d00326998d38a8c4d12dd4dd
Eivind Dagsland Halderaker <Eivind.halderaker@solv.no>
2025-02-17 21:01:09 +0100

    :sparkles: Fix formatting, make it stop and outflow in positive numbers

--- convert.py
+++ convert.py
@@ -2,15 +2,17 @@ import csv
 from datetime import datetime
 
 def convert_csv(input_file, output_file):
-    with open(input_file, mode='r', encoding='utf-8') as infile, open(output_file, mode='w', encoding='utf-8', newline='') as outfile:
-        reader = csv.DictReader(infile, delimiter='\t')
+    with open(input_file, mode='r', encoding='ISO-8859-1') as infile, open(output_file, mode='w', encoding='utf-8', newline='') as outfile:
+        reader = csv.DictReader(infile, delimiter=';')
         fieldnames = ['Date', 'Payee', 'Memo', 'Outflow', 'Inflow']
         writer = csv.DictWriter(outfile, fieldnames=fieldnames)
 
         writer.writeheader()
         for row in reader:
+            if not any(row.values()):  # Check if the row is empty
+                break
             date = datetime.strptime(row['Utført dato'], '%d.%m.%Y').strftime('%Y-%m-%d')
-            outflow = row['Beløp ut']
+            outflow = abs(float(row['Beløp ut'])) if row['Beløp ut'] else ''
             inflow = row['Beløp inn']
             writer.writerow({
                 'Date': date,
